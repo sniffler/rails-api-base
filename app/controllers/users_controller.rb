bd69cc4e05eedc760ecc48a791c62088747faa53
Nebojsa Zoric <office@kolosek.com>
2022-04-03 00:18:32 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,10 @@ class UsersController < ViewController
       user.password_digest = user.reset_password_digest
       user.reset_password_token = nil
       user.reset_password_digest = nil
+            user.confirmed_at = DateTime.current
+      user.confirmation_token = nil
+            user.confirmed_at = DateTime.current
+      user.confirmation_token = nil
       user.save
       render '/users/reset_confirmed'
     else
